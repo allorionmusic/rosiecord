89ec1f696694ee4bccf5e044da45e109c59a2f7d
Skawtnyc <43423200+Skawtnyc@users.noreply.github.com>
2024-10-15 18:53:39 -0400

    Build 65399

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_251.0_65385.ipa";
+Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_252.0_65399.ipa";
 export { Shell, Colors, Divider, States, Constants };
